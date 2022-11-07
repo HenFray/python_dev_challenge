18aaeaf3bfb94bee695433cd5feaf8da15e38d35
Mauro Franetovich <mauro.franetovich@gmail.com>
2022-11-07 17:53:34 -0300

    Finish

--- ejercicio2.py
+++ ejercicio2.py
@@ -20,17 +20,16 @@ _PRODUCT_DF = pd.DataFrame(
 
 
 def is_product_available(product_name, quantity):
+  stock = _PRODUCT_DF[(_PRODUCT_DF['product_name'] == product_name) & (_PRODUCT_DF['quantity'] >= quantity)]
+  return len(stock) > 0
 
-  if quantity > 0:
-    return "HAY STOCK"
-  else:
-    return "NO HAY STOCK"
-'''buscar una funcion en pandas que busque '''
 
 # <<<<<<<<<<<<<<<<<<<MAIN>>>>>>>>>>>>>>>>>>>>>>
 
 if __name__ == "__main__":
-  print("Buenos dias tenemos lo siguentes helados para pedir:")
+  print(is_product_available("Chocolate", 4))
+  print(is_product_available("Chocolate", 2))
+'''  print("Buenos dias tenemos lo siguentes helados para pedir:")
   print("#############################")
 
   _PRODUCT_DF = _PRODUCT_DF.rename(columns={'product_name':'Nombre','quantity': 'Cantidad'})
@@ -47,5 +46,5 @@ if __name__ == "__main__":
   vartest=busqueda['Cantidad']
 
   StockExistente = is_product_available(eleccion_cliente, vartest)
-  print(StockExistente)
+  print(StockExistente)'''
 
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import pandas as pd
+
+# LOCAL
 from ejercicio1 import GeoAPI
-#from ejercicio2 
+from ejercicio2 import is_product_available
 from ejercicio3 import validate_discount_code
 
 
@@ -9,6 +12,39 @@ else:
     print("Bienvenida 2")
 
 # ACA VA EL EJERCICIO 2
+print('Que sabor queres tenemos: ')
+
+_PRODUCT_DF = pd.DataFrame(
+{
+  "product_name": [
+    "Chocolate",
+    "Granizado",
+    "Limon",
+    "Dulce de Leche"
+  ],
+  "quantity": [
+    3,
+    10,
+    0,
+    5
+  ]
+}
+)
+
+print(_PRODUCT_DF.rename(columns={'product_name': 'Nombre', 'quantity': 'Cant'}).to_string(index=False))
+
+condicional = True
+while condicional:
+    sabor = input("Que sabor queres? ")
+    cantidad = int(input(f"Cuantos de {sabor} queres? "))
+    is_product_available(sabor, cantidad)
+
+    if is_product_available(sabor, cantidad) == True:
+        break
+    else:
+        print("No tenemos stock suficiente para esa cantidad!")
+        pass
+
 
 while True:
     code = input("Codigo de descuento: ")
